98e18c467444175b5868ba85684f5f616a9de12d
BUNNYBISANDA <150602663+BUNNYBISANDA@users.noreply.github.com>
2026-05-28 16:15:54 +0700

    Set GitHub Pages base for lululemonvog

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/LululemonAnalytics/',
+  base: command === 'serve' ? '/' : '/lululemonvog/',
   plugins: [react(), tailwindcss()],
 }))
